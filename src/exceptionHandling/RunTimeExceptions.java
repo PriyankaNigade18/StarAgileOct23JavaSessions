479cbce3c6179cb26554cf08df2a49b622c97402
BeizzTech <piyu1818@gmail.com>
2023-12-01 22:30:49 +0530

    1-12-2023 Java Exception Handling

--- src/exceptionHandling/RunTimeExceptions.java
+++ src/exceptionHandling/RunTimeExceptions.java
@@ -10,55 +10,73 @@ public class RunTimeExceptions
 	public static void main(String[] args)
 	{
 		
-		RunTimeExceptions  r1=new RunTimeExceptions ();
-		r1=null;
+		RunTimeExceptions  r1=null;
+		try {
 		System.out.println("Id is: "+r1.id);//NullPointerException
+		}catch(NullPointerException n)
+		{
+			System.out.println("Current class object is null");
+		}
+		
+		
+		
+		
+
+//		String s1="100AB";
+//		try {
+//		int num=Integer.parseInt(s1);//NumberFormatException
+//		System.out.println(num);
+//		}catch(NumberFormatException n)
+//		{
+//			System.out.println("Please provid valid parsable string ");
+//		}
+//		
 		
 		
 		
 		
 		
-/*
-		String s1="100ABC";
-		int num=Integer.parseInt(s1);//NumberFormatException
-		System.out.println(num);
 		
 		
 		
 		
 		
 		
-		
-		
-		
-		
-		/*
-			Scanner sc=new Scanner(System.in);
-			System.out.println("Enter index between 0 to 5");
-			int index=sc.nextInt();
+//			Scanner sc=new Scanner(System.in);
+//			System.out.println("Enter index between 0 to 5");
+//			int index=sc.nextInt();
+//			
+//			
+//			int arr[]= {10,20,30,40,50,60};
+//			try {
+//			System.out.println(arr[index]);//ArrayIndexOutOfBoundsException
+//			}catch(Exception e)
+//			{
+//				System.out.println("Please provide valid index from given value");
+//			}
+//			sc.close();
 			
-			
-			int arr[]= {10,20,30,40,50,60};
-			System.out.println(arr[index]);//ArrayIndexOutOfBoundsException
-			
-			sc.close();
-			
 		
 		
 		
-		/*
-			Scanner sc=new Scanner(System.in);
-			System.out.println("Enter two numbers: ");
-			int a=sc.nextInt();
-			int b=sc.nextInt();
+		
+//			Scanner sc=new Scanner(System.in);
+//			System.out.println("Enter two numbers: ");
+//			int a=sc.nextInt();
+//			int b=sc.nextInt();
+//			
+//			System.out.println("Addition is: "+(a+b));
+//			System.out.println("Subtraction is: "+(a-b));
+//			try {
+//			System.out.println("Division is: "+(a/b));//ArithmeticException
+//			}catch(ArithmeticException obj)
+//			{
+//				System.out.println("Please provide number other than 0 Division"+ obj.getMessage());
+//			}
+//			System.out.println("Multiplication is: "+(a*b));
+//			
+//			sc.close();
 			
-			System.out.println("Addition is: "+(a+b));
-			System.out.println("Subtraction is: "+(a-b));
-			System.out.println("Division is: "+(a/b));//ArithmeticException
-			System.out.println("Multiplication is: "+(a*b));
-			
-			sc.close();
-			*/
 			
 			
 			
